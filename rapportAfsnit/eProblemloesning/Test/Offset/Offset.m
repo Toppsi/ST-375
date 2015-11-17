38e9a2a6a0120c3bf8a6f822fdb6e48ae316e4fc
mathiasvassard <mat-o-94@hotmail.com>
2015-11-17 17:25:15 +0100

    test

---
+++ rapportAfsnit/eProblemloesning/Test/Offset/Offset.m
@@ -0,0 +1,52 @@
+clear
+
+%% Konstanter
+Fs = 500;
+Samplelength = 30;
+T = 1/Fs;
+L = Fs*Samplelength;
+t = (0:L-1)*T;
+f = Fs*(0:(L/2))/L;     %Define the frequency domain f
+get(0,'Factory');
+set(0,'defaultfigurecolor',[1,1,1]);
+
+%% Loader data
+Off159V1 = load('159Vmaaling1');
+Off159V2 = load('159Vmaaling2');
+Off159V3 = load('159Vmaaling3');
+Off133V1 = load('133Vmaaling1');
+Off133V2 = load('133Vmaaling2');
+Off133V3 = load('133Vmaaling3');
+Off186V1 = load('186Vmaaling1');
+Off186V2 = load('186Vmaaling2');
+Off186V3 = load('186Vmaaling3');
+Off186V4 = load('186Vmaaling4');
+
+
+%% Gennemsnitsværdi
+Off159V1mean = mean(Off159V1.data)
+Off159V2mean = mean(Off159V2.data)
+Off159V3mean = mean(Off159V3.data)
+Off159V = [Off159V1mean Off159V2mean Off159V3mean];
+Off159Vmean = mean(Off159V)
+
+Off133V1mean = mean(Off133V1.data)
+Off133V2mean = mean(Off133V2.data)
+Off133V3mean = mean(Off133V3.data)
+Off133V = [Off133V1mean Off133V2mean Off133V3mean];
+Off133Vmean = mean(Off133V)
+
+Off186V1mean = mean(Off186V1.data)
+Off186V2mean = mean(Off186V2.data)
+Off186V3mean = mean(Off186V3.data)
+Off186V4mean = mean(Off186V4.data)
+Off186V = [Off186V1mean Off186V2mean Off186V3mean];
+Off186Vmean = mean(Off186V)
+
+%% Plotter tidsgrafen
+figure
+plot(t, Off159V1.data(1:L))
+axis([0 30 -0.5 0.5])
+title('Volt målt inputspænding 1.59V(1. måling)')
+xlabel('Tid i sekunder')
+ylabel('Spænding målt i Volt')
---
+++ rapportAfsnit/eProblemloesning/Test/Opsamling_forstaerker/Op_forstaerker.m
@@ -0,0 +1,49 @@
+clear
+
+%% Konstanter
+Fs = 500;
+Samplelength = 30;
+T = 1/Fs;
+L = Fs*Samplelength;
+t = (0:L-1)*T;
+f = Fs*(0:(L/2))/L;     %Define the frequency domain f
+get(0,'Factory');
+set(0,'defaultfigurecolor',[1,1,1]);
+
+%% Loader data
+For271V1 = load('271Vmaaling1');
+For271V2 = load('271Vmaaling2');
+For271V3 = load('271Vmaaling3');
+For0V1= load('0Vmaaling1');
+For0V2 = load('0Vmaaling2');
+For0V3 = load('0Vmaaling3');
+For266V1 = load('266Vmaaling1');
+For266V2 = load('266Vmaaling2');
+For266V3 = load('266Vmaaling3');
+
+%% Gennemsnitsværdi
+For271V1mean = mean(For271V1.data)
+For271V2mean = mean(For271V2.data)
+For271V3mean = mean(For271V3.data)
+For271V = [For271V1mean For271V2mean For271V3mean];
+For271Vmean = mean(For271V)
+
+For0V1mean = mean(For0V1.data)
+For0V2mean = mean(For0V2.data)
+For0V3mean = mean(For0V3.data)
+For0V = [For0V1mean For0V2mean For0V3mean];
+For0V1mean = mean(For0V)
+
+For266V1mean = mean(For266V1.data)
+For266V2mean = mean(For266V2.data)
+For266V3mean = mean(For266V3.data)
+For266V = [For266V1mean For266V2mean For266V3mean];
+For266Vmean = mean(For266V)
+
+%% Plotter tidsgrafen
+%figure
+%plot(t, Off159V1.data(1:L))
+% axis([0 30 -0.5 0.5])
+% title('Volt målt inputspænding 1.59V(1. måling)')
+% xlabel('Tid i sekunder')
+% ylabel('Spænding målt i Volt')
