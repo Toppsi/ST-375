7c48f23711944b9b0447e560a3ba6efda01025ff
SofieBjoersrud <sofiehelenebjorsrudjensen@h317.aau1x.klient.frb7ab.site.aau.dk>
2015-12-02 11:05:15 +0100

    filter

--- rapportAfsnit/eProblemloesning/Test/filter/lavpas.m
+++ rapportAfsnit/eProblemloesning/Test/filter/lavpas.m
@@ -26,6 +26,9 @@ text(A(indexB),Y_point1,str2,'HorizontalAlignment','right');
 A=data(:,1);
 B=data(:,2);
 
+C = [10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42 44 46 48 50];	
+D = [-0.021 -0.058 -0.138 -0.295 -0.571 -1.030 -1.655 -2.510 -3.556 -4.730 -6.003 -7.318 -8.628 -9.950 -11.209 -12.490 -13.697 -14.953 -15.950 -17.075 -18.077]
+
 x = 10:50;
 
 index = find(A==25);
@@ -33,16 +36,15 @@ Y_point = B(index)
 indexB = find(A==45);
 Y_point1 = B(indexB)
 
+%indexC = find(C==24);
+%Y_point2 = D(indexC)
+%indexD = find(C==44);
+%Y_point3 = D(indexD)
+
 figure
-semilogx(x,B,A(index),Y_point,'',A(index),Y_point1,'')
+semilogx(A,B,'b')
 xlabel('Frekvens (Hz)')
 ylabel('Forstærkning i dB')
 hold on
-index = find(C==25);
-Y_point = B(index)
-indexD = find(C==45);
-Y_point1 = B(indexD)
-semilogx(x,C,B
-
-C = [10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42 44 46 48 50];	
-D = [-0.021 -0.058 -0.138 -0.295 -0.571 -1.030 -1.655 -2.510 -3.556 -4.730 -6.003 -7.318 -8.628 -9.950 -11.209 -12.490 -13.697 -14.953 -15.950 -17.075 -18.077]
+%semilogx(x,D,C(indexC),Y_point2,'',C(indexC),Y_point3,'')
+semilogx(C,D,'r')
